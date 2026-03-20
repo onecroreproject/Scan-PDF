85dbd5e1704f22ca99af0015c9cb757700b14f4f
onecroreproject <1croreprojectsteam@gmail.com>
2026-03-20 16:20:59 +0530

    first commit

--- allinone/settings.py
+++ allinone/settings.py
@@ -10,11 +10,14 @@ SECRET_KEY = 'django-insecure-change-this-in-production-x9$k2m!q@w3e4r5t6y7u8i9o
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'scanpdf.co.in',
+    'www.scanpdf.co.in'
+]
 
-# ALLOWED_HOSTS = ['scanpdf.co.in', 'www.scanpdf.co.in']
- 
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 
